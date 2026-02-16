387fa2e4d2af9453613a3af11efa038b8c17ad26
AlmikSS <ulmasoviskandar7@gmail.com>
2026-02-16 22:13:40 +0300

    Engine Loop

---
+++ Engine/Core/EngineLoop.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+
+var isRunning = true;
+var accumulator = 0.0;
+const double fixedDelta = 1.0 / 50.0;
+
+var stopwatch = new Stopwatch();
+stopwatch.Start();
+
+double previousTime = stopwatch.Elapsed.TotalMilliseconds;
+
+while (isRunning)
+{
+    double currentTime = stopwatch.Elapsed.TotalMilliseconds;
+    var deltaTime = currentTime - previousTime;
+    previousTime = currentTime;
+    
+    accumulator += deltaTime;
+
+    while (accumulator >= fixedDelta)
+    {
+        Console.WriteLine($"Current time: {currentTime} \n Frame time: {deltaTime}");
+        accumulator -= fixedDelta;
+    }
+
+    if (Console.KeyAvailable)
+    {
+        var key = Console.ReadKey(true).Key;
+        if (key == ConsoleKey.Escape)
+            isRunning = false;
+    }
+}
+
+stopwatch.Stop();
+Console.WriteLine($"{stopwatch.ElapsedMilliseconds} ms");
---
+++ Engine/Core/Time.cs
@@ -0,0 +1,6 @@
+﻿namespace Engine.Core;
+
+public static class Time
+{
+    public static double CurrentTime { get; internal set; }
+}
